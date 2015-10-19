7b5f1b9c4d19808fea43ac58044f52d0d84fe836
Hazar <hazar.ilhan@markafoni.com>
2015-10-19 16:28:56 +0300

    Show table view initially in mainview model

--- src/js/MainViewModel.js
+++ src/js/MainViewModel.js
@@ -1,8 +1,7 @@
 goog.provide('app.MainViewModel');
 goog.require('app.base.ViewModel');
-goog.require('app.models.Product');
 goog.require('app.managers.ViewManager');
-goog.require('app.views.ProductList.View');
+goog.require('app.views.Table.View');
 
 
 
@@ -13,15 +12,7 @@ goog.require('app.views.ProductList.View');
 app.MainViewModel = function(){	
 	goog.base(this);
 
-	this.products = ko.observableArray([
-        new app.models.Product('Garlic bread'),
-        new app.models.Product('Pain au chocolat'),
-        new app.models.Product('Seagull spaghetti', 'like') // This one was already 'liked'
-    ]);
-
-	this.productListView = app.vf.createView(this, new app.views.ProductList.View(this.products));
-
-	app.vm.goToView(this.productListView);
+	app.vm.goToView(app.vf.createView(this, new app.views.Table.View()));
 
 	this.exports.push({ref: 'vm', obj: app.vm});
 	this.export();
@@ -35,24 +26,4 @@ goog.inherits(app.MainViewModel, app.base.ViewModel);
  */
 app.MainViewModel.prototype.bindModelEvents = function(){	
 	this.listeners = [];
-	this.listeners.push(goog.events.listen(this, app.components.LikeWidget.ViewModel.EventType.GOOD_DECISION, function(){
-		window.alert('I like');
-		this.products.push(new app.models.Product('Hellim'));
-	}, false, this));
-
-	this.listeners.push(goog.events.listen(this, app.components.LikeWidget.ViewModel.EventType.BAD_DECISION, function(){
-		window.alert('I dislike');
-	}, false, this));
-};
-
-
-/**
- * 
- * @override
- */
-app.MainViewModel.prototype.disposeInternal = function(){
-	this.listeners.forEach(goog.events.unlistenByKey);
-	this.productListView.dispose();
-
-	goog.base(this, 'disposeInternal');
 };
