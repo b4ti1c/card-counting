dfa8e28752ea5697da99fe6ffe858d18197250dc
Hazar <hazar.ilhan@markafoni.com>
2015-10-19 16:40:34 +0300

    Add Card model

---
+++ src/js/models/Card.js
@@ -0,0 +1,32 @@
+goog.provide('app.models.Card');
+goog.require('app.base.Model');
+
+
+
+/**
+ * 
+ * @constructor
+ * @extends {app.base.Model}
+ *
+ * @param {number} number [description]
+ * @param {app.models.Card.Color} color [description]
+ */
+app.models.Card = function(number, color) {
+	goog.base(this);
+
+	this['number'] = this.number = number;
+	this['color'] = this.color = color;
+};
+goog.inherits(app.models.Card, app.base.Model);
+
+
+/**
+ * 
+ * @enum {string}
+ */
+app.models.Card.Color = {
+	HEART: 0,
+	DIAMOND: 1,
+	SPADE: 2,
+	CLUB: 3,
+}
