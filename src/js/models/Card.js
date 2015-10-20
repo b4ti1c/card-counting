fdc9a679482b71add8217db4db6189e5710bc545
Hazar <hazar.ilhan@markafoni.com>
2015-10-20 14:22:32 +0300

    Fix card model

--- src/js/models/Card.js
+++ src/js/models/Card.js
@@ -8,7 +8,7 @@ goog.require('app.base.Model');
  * @constructor
  * @extends {app.base.Model}
  *
- * @param {number} number [description]
+ * @param {app.models.Card.Number} number [description]
  * @param {app.models.Card.Color} color [description]
  */
 app.models.Card = function(number, color) {
@@ -25,8 +25,29 @@ goog.inherits(app.models.Card, app.base.Model);
  * @enum {number}
  */
 app.models.Card.Color = {
-	HEART: 0,
-	DIAMOND: 1,
-	SPADE: 2,
-	CLUB: 3,
+	DIAMOND: 0,
+	HEART: 1,
+	CLUB: 2,
+	SPADE: 3,
+}
+
+
+/**
+ * 
+ * @enum {number}
+ */
+app.models.Card.Number = {
+	2: 1,
+	3: 2,
+	4: 3,
+	5: 4,
+	6: 5,
+	7: 6,
+	8: 7,
+	9: 8,
+	10: 9,
+	J: 10,
+	Q: 11,
+	K: 12,
+	A: 13
 }
