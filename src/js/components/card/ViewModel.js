5240f4da57a928f26b8cd9e2cf3bef1002f98bc1
Hazar <hazar.ilhan@markafoni.com>
2015-10-30 07:47:24 +0200

    Fix centering of hands and table finally

--- src/js/components/card/ViewModel.js
+++ src/js/components/card/ViewModel.js
@@ -15,7 +15,7 @@ app.components.Card.ViewModel = function(params, element){
 
 	if(!this.open) this.open = ko.observable(false);
 	if(!this.inhand) this.inhand = ko.observable(false);
-	if(!this.index) this.index = ko.observable(0);
+	if(!this.index) this.index = ko.observable(6); //middle of a 13-cards hand
 
 	this.moved = ko.observable(false);
 
